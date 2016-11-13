5edd8bcf687b639d03bfd93712beb88a52c02973
dover-xu <xudong_bj@sina.cn>
2016-11-13 22:15:41 +0800

    Completed simple web view

--- focus/views.py
+++ focus/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from focus.forms import LoginForm
+from focus.models import Article
+
+
+def index(request):
+    latest_article_list = Article.objects.query_by_time()
+    loginform = LoginForm()
+    context = {'latest_article_list': latest_article_list, 'loginform': loginform}
+    return render(request, 'index.html', context)
