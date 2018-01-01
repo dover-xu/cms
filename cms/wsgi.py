e5aef9c09328703aacb68e60572685ddfb3895aa
dover-xu <xudong_bj@sina.cn>
2018-01-01 15:35:45 +0800

    feat(wsgi): add newrelic apm

--- cms/wsgi.py
+++ cms/wsgi.py
@@ -8,11 +8,14 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-
+import newrelic.agent
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cms.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application)  # Django自身不能在生产环境提供静态文件服务，WhiteNoise允许Django在不依赖CDN的情况下提供自己的静态文件
+
+newrelic.agent.initialize('newrelic.ini')
+application = newrelic.agent.WSGIApplicationWrapper(application)
